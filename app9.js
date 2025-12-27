334967d29fd8c49eaa6facddcb5e8b50105be485
rinu12nu <rinzhongben74@gmail.com>
2025-12-28 02:56:32 +0900

    コードの修正

--- app9.js
+++ app9.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
-let buki = [
+let splbuki = [
   { id:1, name:"ボールドマーカー", sub:"カーリングボム", special:"ウルトラハンコ" },
   { id:2, name:"ボールドマーカーネオ", sub:"ジャンプビーコン", special:"メガホンレーザー" },
   { id:3, name:"もみじシューター", sub:"トーピード", special:"ホップソナー" },
@@ -19,7 +19,7 @@ let buki = [
 // 一覧
 app.get("/spl", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
-  res.render('spl1', {data: buki} );
+  res.render('spl1', {data: splbuki} );
 });
 
 // Create
@@ -31,7 +31,7 @@ app.get("/spl/create", (req, res) => {
 app.get("/spl/:number", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
   const number = req.params.number;
-  const detail = buki[ number ];
+  const detail = splbuki[ number ];
   res.render('spl_detail', {id: number, data: detail} );
 });
 
@@ -40,27 +40,27 @@ app.get("/spl/delete/:number", (req, res) => {
   // 本来は削除の確認ページを表示する
   // 本来は削除する番号が存在するか厳重にチェックする
   // 本来ならここにDBとのやり取りが入る
-  buki.splice( req.params.number, 1 );
+  splbuki.splice( req.params.number, 1 );
   res.redirect('/spl' );
 });
 
 // Create
 app.post("/spl", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
-  const id = buki.length + 1;
+  const id = splbuki.length + 1;
   const name = req.body.name;
   const sub = req.body.sub;
   const special = req.body.special;
-  buki.push( { id: id, name: name, sub: sub, special: special } );
-  console.log( buki );
-  res.render('spl1', {data: buki} );
+  splbuki.push( { id: id, name: name, sub: sub, special: special } );
+  console.log( splbuki );
+  res.render('spl1', {data: splbuki} );
 });
 
 // Edit
 app.get("/spl/edit/:number", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
   const number = req.params.number;
-  const detail = buki[ number ];
+  const detail = splbuki[ number ];
   res.render('spl_edit', {id: number, data: detail} );
 });
 
@@ -68,23 +68,31 @@ app.get("/spl/edit/:number", (req, res) => {
 app.post("/spl/update/:number", (req, res) => {
   // 本来は変更する番号が存在するか，各項目が正しいか厳重にチェックする
   // 本来ならここにDBとのやり取りが入る
-  buki[req.params.number].name = req.body.name;
-  buki[req.params.number].sub = req.body.sub;
-  buki[req.params.number].special = req.body.special;
-  console.log( buki );
+  splbuki[req.params.number].name = req.body.name;
+  splbuki[req.params.number].sub = req.body.sub;
+  splbuki[req.params.number].special = req.body.special;
+  console.log( splbuki );
   res.redirect('/spl' );
 });
 
 // Random
 app.post("/spl/random", (req, res) => {
-    const spl = Math.floor( Math.random() * buki.length );
-    const randomname = buki[ spl ].name;
-    res.render( 'spl1', {data: buki, randomresult: randomname} );
+    const spl = Math.floor( Math.random() * splbuki.length );
+    const randomname = splbuki[ spl ].name;
+    res.render( 'spl1', {data: splbuki, randomresult: randomname} );
 });
 
 let animal = [
     { id:1, name:"1ごう", type:"ネコ", character:"ハキハキ", birthday:"8月1日" },
-    
+    { id:2, name:"ジャスミン", type:"コグマ", character:"普通", birthday:"5月18日" },
+    { id:3, name:"ジャック", type:"ネコ", character:"キザ", birthday:"10月1日" },
+    { id:4, name:"ジュン", type:"リス", character:"キザ", birthday:"9月29日" },
+    { id:5, name:"ちゃちゃまる", type:"ヒツジ", character:"ハキハキ", birthday:"3月18日" },
+    { id:6, name:"みすず", type:"コグマ", character:"オトナ", birthday:"3月10日" },
+    { id:7, name:"ミッチェル", type:"ウサギ", character:"ぼんやり", birthday:"5月19日" },
+    { id:8, name:"ラムネ", type:"ネコ", character:"普通", birthday:"3月27日" },
+    { id:9, name:"リリアン", type:"ウサギ", character:"元気", birthday:"5月9日" },
+    { id:10, name:"ロボ", type:"オオカミ", character:"コワイ", birthday:"11月25日" },
   ];
   
   // 一覧
@@ -150,7 +158,14 @@ let animal = [
   
   let youkai = [
     { id:1, name:"ジバニャン", type:"プリチー", rank:"D", skill:"まえのめり", soultimate:"ひゃくれつ肉球" },
-    
+    { id:2, name:"コマさん", type:"プリチー", rank:"D", skill:"「あ」", soultimate:"ひとだま乱舞" },
+    { id:3, name:"メラメライオン", type:"イサマシ", rank:"C", skill:"もえるとうし", soultimate:"正拳バーニング" },
+    { id:4, name:"キュウビ", type:"フシギ", rank:"S", skill:"きゅうかく", soultimate:"紅蓮地獄" },
+    { id:5, name:"オロチ", type:"ニョロロン", rank:"S", skill:"オロチャージ", soultimate:"やまたのおろち" },
+    { id:6, name:"イヌニャン", type:"ゴーケツ", rank:"B", skill:"きびだんご", soultimate:"犬猫ワンニャン肉球" },
+    { id:7, name:"キズナース", type:"ポカポカ", rank:"B", skill:"きずなめ", soultimate:"ナメナメ集中治療" },
+    { id:8, name:"魔ガサス", type:"ウスラカゲ", rank:"A", skill:"おれないハート", soultimate:"魔ガサスショック" },
+    { id:9, name:"トホホギス", type:"ブキミー", rank:"E", skill:"風あそび", soultimate:"トホホの砲" },
   ];
   
   // 一覧
@@ -190,7 +205,7 @@ let animal = [
     const rank = req.body.rank;
     const skill = req.body.skill;
     const soultimate = req.body.soultimate;
-    animal.push( { id: id, name: name, type: type, rank: rank, skill: skill, soultimate: soultimate } );
+    youkai.push( { id: id, name: name, type: type, rank: rank, skill: skill, soultimate: soultimate } );
     console.log( youkai );
     res.render('youkai1', {data: youkai} );
   });
